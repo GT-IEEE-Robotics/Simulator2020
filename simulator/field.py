54fa7afebad65e79d56cfe43378c66338f1d51dd
Ammar Ratnani <ammrat13@gmail.com>
2019-11-07 15:18:58 -0500

    Made changes requested by Binit

--- simulator/field.py
+++ simulator/field.py
@@ -8,7 +8,7 @@ Last Modified: Ammar on 11/06
 import pybullet as p
 
 from simulator.utilities import Utilities
-from simulator.field_components.buttons import Buttons
+from simulator.buttons import Buttons
 
 class Field:
     """The Field class maintains the state of the simulated elements within the field"""
@@ -26,5 +26,5 @@ class Field:
         self.model_id = p.loadURDF(Utilities.gen_urdf_path("field/field.urdf", cwd), useFixedBase=True)
         self.buttons.populate_joint_ids([1, 3, 5, 7, 9, 11, 13, 15, 17, 19])
 
-        for i in range(1, 21, 2):
-            p.setJointMotorControl2(self.field_model_id, i, controlMode=p.POSITION_CONTROL, targetPosition=0.0, force=0.2, positionGain=0.8)
+        for b in self.buttons:
+            p.setJointMotorControl2(self.model_id, b.joint_id, controlMode=p.POSITION_CONTROL, targetPosition=0.0, force=0.2, positionGain=0.8)
