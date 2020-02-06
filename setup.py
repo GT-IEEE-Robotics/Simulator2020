5f10eaa0ecf50eca78ee9eb092497cb4e40fdd09
Binit Shah <binitpshah@gmail.com>
2020-02-05 19:36:58 -0500

    setuptools script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "simulator2020",
+    version = "1.0.0",
+    author = "GT IEEE Robotics",
+    author_email = "ieee_president@gmail.com",
+    description = ("The simulation environment for the 2020 SoutheastCon robotics competition."),
+    url = "https://github.com/GT-IEEE-Robotics/Simulator2020",
+    packages=['simulator', 'tests'],
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.5',
+    install_requires=['numpy',
+                      'pybullet']
+)
